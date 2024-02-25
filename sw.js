b3faa805aac610ff873a68063a61b46440c8f3ef
Dirk Arnez <smalldirkalex@gmail.com>
2024-02-25 21:15:59 +0800

    - update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/7.0.0/workbox-s
 
 workbox.routing.registerRoute(
   new RegExp('https://api\.github\.com/repos/'),
-  workbox.strategies.CacheFirst()
+  new workbox.strategies.CacheFirst()
 );
 
 // self.addEventListener('message', (event) => {
