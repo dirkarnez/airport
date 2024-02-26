acda8a921630fdecb6e0dd5c77cc31fda6fba9a1
Dirk Arnez <smalldirkalex@gmail.com>
2024-02-26 14:05:42 +0800

    - update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,7 @@
 importScripts('https://cdnjs.cloudflare.com/ajax/libs/workbox-sw/7.0.0/workbox-sw.min.js');
 
 
-workbox.core.setConfig({
-  clientsClaim: true
-});
+workbox.core.clientsClaim();
 
 self.addEventListener('install', (event) => {
   self.skipWaiting();
