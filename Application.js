0aabfe9135e44430d7c371cd49dbb693d123cbfd
Dirk Arnez <smalldirkalex@gmail.com>
2025-02-13 13:35:03 +0800

    - update Application.js

--- Application.js
+++ Application.js
@@ -36,7 +36,7 @@ function Application({entryPoints}) {
                   <a href="https://pvrp-sao.polyu.edu.hk/student/login">Job Seeker Login | PolyU VRP</a><br />
                   <a href="https://www.polyu.edu.hk/cfso/docdrive/_sso/services/both/lostandfound.html">Lost property</a>
                   <a href="https://www.hangseng.com/zh-hk/security">eBanking</a>
-                  <hr>
+                  <hr/>
                   <div>
                         <a href="https://www.bmf.edu.hk/">佛教茂峰法師紀念中學</a><br />
                         <a href="https://maps.app.goo.gl/yPqhY2ZaQwGQfDth9">map</a><br />
