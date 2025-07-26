203d415f5463ef3c694f38486e2853848cb5bab5
Dirk Arnez <smalldirkalex@gmail.com>
2025-07-26 22:09:42 +0800

    - update OnlineDocViewer.js

--- OnlineDocViewer.js
+++ OnlineDocViewer.js
@@ -7,6 +7,8 @@ browser version of babel do all the work for us.
 [Embedded File Viewer: Google Drive, OneDrive](https://gist.github.com/tzmartin/1cf85dc3d975f94cfddc04bc0dd399be)
 [Embeddable Google Document Viewer](https://googlesystem.blogspot.com/2009/09/embeddable-google-document-viewer.html)
 [快速在網頁上預覽 Office 檔案](https://blog.poychang.net/office-word-excel-powerpoint-online-viewer/)
+https://view.officeapps.live.com/op/view.aspx?src=https://github.com/poychang/blog.poychang.net/raw/master/assets/post-files/THIS-IS-WORD.docx
+https://docs.google.com/viewer?url=https://github.com/poychang/blog.poychang.net/raw/master/assets/post-files/THIS-IS-WORD.docx
 */
 function OnlineDocViewer() {
 	const [ url, setUrl ] = React.useState("https://www.mtr.com.hk/digitalleaflet/data/upload/ch/article/pdf/WEK-leaflet-Digital-202506_1750939c6496.pdf");
