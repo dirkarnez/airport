633ca96b297eb48ce9e6d15b76159155c915f0bf
Dirk Arnez <smalldirkalex@gmail.com>
2025-07-26 22:05:21 +0800

    - update OnlineDocViewer.js

--- OnlineDocViewer.js
+++ OnlineDocViewer.js
@@ -2,6 +2,12 @@
 "module.exports" / "export default" is not needed.
 browser version of babel do all the work for us.
 */
+
+/*
+[Embedded File Viewer: Google Drive, OneDrive](https://gist.github.com/tzmartin/1cf85dc3d975f94cfddc04bc0dd399be)
+[Embeddable Google Document Viewer](https://googlesystem.blogspot.com/2009/09/embeddable-google-document-viewer.html)
+[快速在網頁上預覽 Office 檔案](https://blog.poychang.net/office-word-excel-powerpoint-online-viewer/)
+*/
 function OnlineDocViewer() {
 	const [ url, setUrl ] = React.useState("https://www.mtr.com.hk/digitalleaflet/data/upload/ch/article/pdf/WEK-leaflet-Digital-202506_1750939c6496.pdf");
 
