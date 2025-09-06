62396da5240bb5ba943930756fce402b45f11fff
Dirk Arnez <smalldirkalex@gmail.com>
2025-09-07 01:25:24 +0800

    - add Escape.js

---
+++ Escape.js
@@ -0,0 +1,11 @@
+/* 
+"module.exports" / "export default" is not needed.
+browser version of babel do all the work for us.
+*/
+
+/*
+[component/escape-html: Escape string for use in HTML](https://github.com/component/escape-html)
+*/
+function Escape() {
+  return ()
+}
