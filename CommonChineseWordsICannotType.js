0f922dc13b110c25e37a5cd256f0aadd8bec2657
Dirk Arnez <smalldirkalex@gmail.com>
2025-09-15 18:39:21 +0800

    - update CommonChineseWordsICannotType.js

--- CommonChineseWordsICannotType.js
+++ CommonChineseWordsICannotType.js
@@ -30,6 +30,7 @@ function CommonChineseWordsICannotType() {
           { word: "尹", howto: "尸大" },
           { word: "肉", howto: "人人" },
           { word: "片", howto: "中中" },
+          { word: "敷衍", howto: "戈大" },
           { word: "面面相覷", howto: "卜山" },
           { word: "凹", howto: "尸山" },
           { word: "凸", howto: "月尸" },
