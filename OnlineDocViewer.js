676cef5cb0b4762a4f0013193eb28b4c36377ef9
Dirk Arnez <smalldirkalex@gmail.com>
2025-09-19 19:00:25 +0800

    - update OnlineDocViewer.js

--- OnlineDocViewer.js
+++ OnlineDocViewer.js
@@ -11,17 +11,19 @@ https://view.officeapps.live.com/op/view.aspx?src=https://github.com/poychang/bl
 https://docs.google.com/viewer?url=https://github.com/poychang/blog.poychang.net/raw/master/assets/post-files/THIS-IS-WORD.docx
 */
 function OnlineDocViewer() {
-	const [ url, setUrl ] = React.useState("https://www.mtr.com.hk/digitalleaflet/data/upload/ch/article/pdf/WEK-leaflet-Digital-202506_1750939c6496.pdf");
+	const [ url, setURL ] = React.useState("https://www.mtr.com.hk/digitalleaflet/data/upload/ch/article/pdf/WEK-leaflet-Digital-202506_1750939c6496.pdf");
 
   	const handleChange = (event) => {
-		setUrl(event.target.value);
+		setURL(event.target.value);
 	};
   
-        return (
+    return (
 		<React.Fragment>
 			<input type="text" value={url} onChange={handleChange} style={{width: "100%"}}/>
 			<br/>
+			<button onClick={() => setURL(`https://www.polyu.edu.hk/cfso/docdrive/Operation_mode_and_opening_hours_of_campus_catering_outlets.pdf`)}>PolyU canteens</button>
+			<br/>
 			<iframe src={`https://docs.google.com/gview?embedded=true&url=${url}`} style={{width: "100%", height: "1000px"}} frameborder="0"></iframe>
 		</React.Fragment>
-        )
+    )
 }
