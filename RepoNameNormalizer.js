e4a34a31f9cb298125f8091c864f56a5398b6e37
Dirk Arnez <smalldirkalex@gmail.com>
2025-10-07 04:39:03 +0800

    - add RepoNameNormalizer.js

---
+++ RepoNameNormalizer.js
@@ -0,0 +1,20 @@
+/* 
+"module.exports" / "export default" is not needed.
+browser version of babel do all the work for us.
+*/
+
+/*
+.replaceAll("/", "-").replaceAll("_", "-").toLocaleLowerCase()
+*/
+
+function RepoNameNormalizer() {
+  // const divRef = React.useRef(null);
+  //  function handleClick() {
+  //   divRef.current.innerHTML = "Chris&apos; corner";
+  //   console.log(divRef.current.innerText)
+  // }
+  //    <div ref={divRef} />
+  return (
+    <TextInputComponent/>
+  )
+}
