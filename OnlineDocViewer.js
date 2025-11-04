2b03071bdcc7e0cea370e05a21c45ada9ffea8d0
Dirk Arnez <smalldirkalex@gmail.com>
2025-11-04 19:57:23 +0800

    - update OnlineDocViewer.js

--- OnlineDocViewer.js
+++ OnlineDocViewer.js
@@ -22,11 +22,11 @@ function OnlineDocViewer() {
 			<h5><a href="https://github.com/dirkarnez/airport/blob/main/OnlineDocViewer.js" target="_blank">Online Doc Viewer</a></h5>
 			<input type="text" value={url} onChange={handleChange} style={{width: "100%"}}/>
 			<br/>
-			<button type="button" onClick={() => setURL(`https://www.polyu.edu.hk/cfso/docdrive/Operation_mode_and_opening_hours_of_campus_catering_outlets.pdf`)}>PolyU canteens</button>
-			<button type="button" onClick={() => setURL(`https://www.mtr.com.hk/digitalleaflet/data/upload/en/article/pdf/WEK-leaflet-Digital-202506_175099b64I96.pdf`)}>High Speed Rail Leaflet</button>
-			<button type="button" onClick={() => setURL(`https://www.mtr.com.hk/digitalleaflet/data/upload/en/article/pdf/DIH_17459803p3n8.pdf`)}>Diamond Hill Leaflet</button>
-			<button type="button" onClick={() => setURL(`https://www.mtr.com.hk/digitalleaflet/data/upload/en/article/pdf/Digital_Station_information_leaflet_WTS_Mar_2024_171108O95980.pdf`)}>Wong Tai Sin Leaflet</button>
-			<button type="button" onClick={() => setURL(`https://www.mtr.com.hk/digitalleaflet/data/upload/en/article/pdf/HUH_Digital_Leaflet_032025_174n29584r99.pdf`)}>Hung Hom Leaflet</button>
+			<button type="button" className="btn btn-primary" onClick={() => setURL(`https://www.polyu.edu.hk/cfso/docdrive/Operation_mode_and_opening_hours_of_campus_catering_outlets.pdf`)}>PolyU canteens</button>
+			<button type="button" className="btn btn-primary" onClick={() => setURL(`https://www.mtr.com.hk/digitalleaflet/data/upload/en/article/pdf/WEK-leaflet-Digital-202506_175099b64I96.pdf`)}>High Speed Rail Leaflet</button>
+			<button type="button" className="btn btn-primary" onClick={() => setURL(`https://www.mtr.com.hk/digitalleaflet/data/upload/en/article/pdf/DIH_17459803p3n8.pdf`)}>Diamond Hill Leaflet</button>
+			<button type="button" className="btn btn-primary" onClick={() => setURL(`https://www.mtr.com.hk/digitalleaflet/data/upload/en/article/pdf/Digital_Station_information_leaflet_WTS_Mar_2024_171108O95980.pdf`)}>Wong Tai Sin Leaflet</button>
+			<button type="button" className="btn btn-primary" onClick={() => setURL(`https://www.mtr.com.hk/digitalleaflet/data/upload/en/article/pdf/HUH_Digital_Leaflet_032025_174n29584r99.pdf`)}>Hung Hom Leaflet</button>
 			<br/>
 			{ !!url && <iframe src={`https://docs.google.com/gview?embedded=true&url=${url}`} style={{width: "100%", height: "1000px"}} frameborder="0"></iframe> }
 		</React.Fragment>
