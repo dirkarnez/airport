e3270db629613a5668eb3c1d165505d762911b5a
Dirk Arnez <smalldirkalex@gmail.com>
2025-11-19 17:33:54 +0800

    - update OnScreenRuler.js

--- OnScreenRuler.js
+++ OnScreenRuler.js
@@ -6,10 +6,7 @@ function OnScreenRuler() {
         height: "calc(72pt / 2.54)",
         borderWidth: 1,
         borderStyle: "solid dashed",
-        lineHeight: "24pt",
-        fontSize: "19pt",
-        textAlign: "center",
-        margin: "1.2em"
+        textAlign: "center"
       }}
     >
       ↑<br />
