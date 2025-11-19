4408f010b8146e4e50db8ca7bff56e88f3d2ab98
Dirk Arnez <smalldirkalex@gmail.com>
2025-11-19 16:01:09 +0800

    - update OnScreenRuler.js

--- OnScreenRuler.js
+++ OnScreenRuler.js
@@ -1,5 +1,20 @@
 function OnScreenRuler() {
   return (
-    <span style="display: block;height: calc(72pt / 2.54);border-width: 1px;border-style: solid dashed;line-height: 24pt;font-size: 19pt;text-align: center;margin: 1.2em;">↑<br>72pt  / 2.54<br>↓</span>
+   <span
+      style={{
+        display: "block",
+        height: "calc(72pt / 2.54)",
+        borderWidth: 1,
+        borderStyle: "solid dashed",
+        lineHeight: "24pt",
+        fontSize: "19pt",
+        textAlign: "center",
+        margin: "1.2em"
+      }}
+    >
+      ↑<br />
+      72pt / 2.54
+      <br />↓
+    </span>
   )
 }
