bb83644f99c51e783ed12b2e2e7e54a425ca5036
Dirk Arnez <smalldirkalex@gmail.com>
2025-11-19 15:56:00 +0800

    - add OnScreenRuler.js

---
+++ OnScreenRuler.js
@@ -0,0 +1,5 @@
+function OnScreenRuler() {
+  return (
+    <span style="display: block;height: calc(72pt / 2.54);border-width: 1px;border-style: solid dashed;line-height: 24pt;font-size: 19pt;text-align: center;margin: 1.2em;">↑<br>72pt  / 2.54<br>↓</span>
+  )
+}
