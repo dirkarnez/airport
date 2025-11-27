d0d7a5b767119fcaca2791a8eda2bd903fe8f5b0
Dirk Arnez <smalldirkalex@gmail.com>
2025-11-27 14:45:09 +0800

    - update TabletCalculator.js

--- TabletCalculator.js
+++ TabletCalculator.js
@@ -2,7 +2,7 @@
     // calculate the minimum tablets needed to split so that all tablets are used
     // 3/4 tablet here
     let tab = NaN;
-    let i = NaN;
-    for (i = 1; (Number.isInteger((tab = (3/4) * i))) != true; i++);
+    let i = 1;
+    while (Number.isInteger((tab = (3/4) * ++i)) != true);
     console.log(`${tab} for ${i} day(s)`)
 })();
