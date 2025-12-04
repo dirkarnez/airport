c5b0e98c25023ce44a7407250c3df704a7e687f1
Dirk Arnez <smalldirkalex@gmail.com>
2025-12-04 16:52:07 +0800

    - update Name.js

--- Name.js
+++ Name.js
@@ -5,21 +5,26 @@ browser version of babel do all the work for us.
 
 function Div({children}) {
   return (
-    <div style={{ border: "1px solid black", padding: "20px", fontSize: "1rem", textAlign: "center" }}>{children}</div>
+    <div style={{ border: "1px solid black", fontSize: "1rem", textAlign: "center" }}>{ children }</div>
   )
 }
 
 function Name() {
   return (
-    <div style={{ display: "grid", gridTemplateColumns: "auto auto", border: "1px solid black" }}>
-      <Div>姓</Div>
-      <Div>名</Div>
-      <Div>Last name</Div>  
-      <Div>First name</Div>
-      <Div>Surname</Div>
-      <Div>Given name</Div>  
-      <Div>Family name</Div>
-      <Div></Div>
-    </div>
+    <React.Fragment>
+      <h5><a href="https://github.com/dirkarnez/airport/blob/main/Name.js" target="_blank">Name</a></h5>
+      <a href="https://www.hkcards.com" target="_blank">hkcards 倉頡字典</a>
+      <br/>
+      <div style={{ display: "grid", gridTemplateColumns: "auto auto", border: "1px solid black" }}>
+        <Div>姓</Div>
+        <Div>名</Div>
+        <Div>Last name</Div>  
+        <Div>First name</Div>
+        <Div>Surname</Div>
+        <Div>Given name</Div>  
+        <Div>Family name</Div>
+        <Div></Div>
+      </div>
+    </React.Fragment>
   )
 }
