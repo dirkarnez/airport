dd62adf5d13196f995ad3cf01791cba7eecd77bc
Dirk Arnez <smalldirkalex@gmail.com>
2025-12-24 16:55:15 +0800

    - add RichManCalculator.js

---
+++ RichManCalculator.js
@@ -0,0 +1,47 @@
+/* 
+"module.exports" / "export default" is not needed.
+browser version of babel do all the work for us.
+*/
+
+const TextInputComponent = () => {
+  const [ inputValue, setInputValue ] = React.useState('');
+  const [ transformedValue, setTransformedValue ] = React.useState("");
+
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  
+  React.useEffect(() => {
+    setTransformedValue(!!inputValue ? inputValue.replaceAll("/", "-").replaceAll("_", "-").toLocaleLowerCase() : "");
+  }, [ inputValue ]);
+
+  return (
+    <React.Fragment>
+      <label htmlfor="input">Input</label>
+      <br/>
+      <input
+        name="input"
+        type="text"
+        style={{width: "100%"}}
+        value={inputValue}
+        onChange={handleChange}
+      />
+      <p>Transformed: {transformedValue}</p>
+    </React.Fragment>
+  );
+};
+
+function RichManCalculator() {
+  // const divRef = React.useRef(null);
+  //  function handleClick() {
+  //   divRef.current.innerHTML = "Chris&apos; corner";
+  //   console.log(divRef.current.innerText)
+  // }
+  //    <div ref={divRef} />
+  return (
+    <React.Fragment>
+      <h5>Rich Man Calculator</h5>
+      <TextInputComponent/>
+    </React.Fragment>
+  )
+}
