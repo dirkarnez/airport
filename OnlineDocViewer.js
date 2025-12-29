dad31650e36f38e3d46408c1ea64d3cfaca29dda
Dirk Arnez <smalldirkalex@gmail.com>
2025-12-30 01:44:34 +0800

    - update OnlineDocViewer.js

--- OnlineDocViewer.js
+++ OnlineDocViewer.js
@@ -11,6 +11,20 @@ https://view.officeapps.live.com/op/view.aspx?src=https://github.com/poychang/bl
 https://docs.google.com/viewer?url=https://github.com/poychang/blog.poychang.net/raw/master/assets/post-files/THIS-IS-WORD.docx
 */
  /* className="d-flex justify-content-around"*/
+
+const typedPDFs = Object.groupBy([
+	{ url: "https://www.ti.com/lit/ds/symlink/lm386.pdf", name: "LM386", type: "Datasheet" },
+	{ url: "https://www.polyu.edu.hk/cfso/docdrive/Operation_mode_and_opening_hours_of_campus_catering_outlets.pdf", name: "PolyU canteens", type: "Timetable" },
+	{ url: "https://www.mtr.com.hk/digitalleaflet/data/upload/en/article/pdf/WEK-leaflet-Digital-202506_175099b64I96.pdf", name: "High Speed Rail Leaflet", type: "Map" },
+	{ url: "https://www.mtr.com.hk/digitalleaflet/data/upload/en/article/pdf/DIH_17459803p3n8.pdf", name: "Diamond Hill Leaflet", type: "Map" },
+	{ url: "https://www.mtr.com.hk/digitalleaflet/data/upload/en/article/pdf/Digital_Station_information_leaflet_WTS_Mar_2024_171108O95980.pdf", name: "Wong Tai Sin Leaflet", type: "Map" },
+	{ url: "https://www.mtr.com.hk/digitalleaflet/data/upload/en/article/pdf/HUH_Digital_Leaflet_032025_174n29584r99.pdf", name: "Hung Hom Leaflet", type: "Map" },
+	{ url: "https://ngspice.sourceforge.io/docs/ngspice-manual.pdf", name: "Manual", type: "map" }
+], ({ type }) =>
+  type
+);
+
+
 function OnlineDocViewer() {
 	const [ url, setURL ] = React.useState(``);
 
@@ -24,12 +38,20 @@ function OnlineDocViewer() {
 			<input type="text" value={url} onChange={handleChange} style={{width: "100%"}}/>
 			<br/>
 			<div>
-				<button type="button" className="btn btn-primary" onClick={() => setURL(`https://www.polyu.edu.hk/cfso/docdrive/Operation_mode_and_opening_hours_of_campus_catering_outlets.pdf`)}>PolyU canteens</button>
-				<button type="button" className="btn btn-primary" onClick={() => setURL(`https://www.ti.com/lit/ds/symlink/lm386.pdf`)}>LM386</button>
-				<button type="button" className="btn btn-primary" onClick={() => setURL(`https://www.mtr.com.hk/digitalleaflet/data/upload/en/article/pdf/WEK-leaflet-Digital-202506_175099b64I96.pdf`)}>High Speed Rail Leaflet</button>
-				<button type="button" className="btn btn-primary" onClick={() => setURL(`https://www.mtr.com.hk/digitalleaflet/data/upload/en/article/pdf/DIH_17459803p3n8.pdf`)}>Diamond Hill Leaflet</button>
-				<button type="button" className="btn btn-primary" onClick={() => setURL(`https://www.mtr.com.hk/digitalleaflet/data/upload/en/article/pdf/Digital_Station_information_leaflet_WTS_Mar_2024_171108O95980.pdf`)}>Wong Tai Sin Leaflet</button>
-				<button type="button" className="btn btn-primary" onClick={() => setURL(`https://www.mtr.com.hk/digitalleaflet/data/upload/en/article/pdf/HUH_Digital_Leaflet_032025_174n29584r99.pdf`)}>Hung Hom Leaflet</button>
+				{
+					Object.keys(typedPDFs)
+					.map(type => {
+						const pdfList = typedPDFs[type];
+						return (
+							<details>
+								<summary>{type}</summary>
+								{
+									pdfList.map(pdf => <button type="button" className="btn btn-primary" onClick={() => setURL(pdf.url)}>{pdf.name}</button>)
+								}
+							</details>
+						);
+					})
+				}
 			</div>
 			<br/>
 			{ 
