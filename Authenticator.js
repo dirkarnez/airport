059c971be23d1662bce4730e1500d195609e9862
Dirk Arnez <smalldirkalex@gmail.com>
2026-09-06 20:29:13 +0800

    - update Authenticator.js

--- Authenticator.js
+++ Authenticator.js
@@ -10,7 +10,7 @@ function Authenticator() {
             <h5>
                 <a href="https://github.com/dirkarnez/airport/blob/main/Netvigator.js" target="_blank">Authenticator</a>
             </h5>
-            <span>{CryptoJS.AES.decrypt("2FsdGVkX1+aMbg2yzwr7e7ykGFZ/u/X0ScfZHgadisCCeKOolPj6ZeQvplzAbIN", (Cookies.get(window.key) || "")).toString(CryptoJS.enc.Utf8) }</span>
+            <span>{CryptoJS.AES.decrypt("U2FsdGVkX19awg7dFkjJiwsC+VCVSe+yeG/MVphxqeCm5MLAwPqmPO1cszORCOK8", (Cookies.get(window.key) || "")).toString(CryptoJS.enc.Utf8) }</span>
             <br/>
         </React.Fragment>
     )
