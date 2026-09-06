0bf7cb8b4a93d0d0097fa8360c8d60225b7fb622
Dirk Arnez <smalldirkalex@gmail.com>
2026-09-06 20:23:15 +0800

    - update Authenticator.jsx

--- Authenticator.jsx
+++ Authenticator.jsx
@@ -1,15 +1,17 @@
-    window.decrypted = CryptoJS.AES.decrypt("U2FsdGVkX19GXXB439Li0ik+eJvU6pP3vUJ9cQXOfAm7GTAl8SiujlHUIPwFwyFOKaRept6Ekze2o7BCTgzVUA==", (Cookies.get(window.key) || "")).toString(CryptoJS.enc.Utf8);
-    window.myOctokit = new octokitCore.Octokit({ auth: window.decrypted });
-
 function Authenticator() {
-  return (
-    <React.Fragment>
-      <h5>
-        <a href="https://github.com/dirkarnez/airport/blob/main/Netvigator.js" target="_blank">Netvigator</a>
-      </h5>
-      <span>68-110012-0861-59 (Bill type 62)</span>
-      <br/>
-      <img src={dataURI} alt="" style={{ width: "100%" }}/>
-    </React.Fragment>
-  )
+    // React.useEffect(() => {
+    //     return () => {
+    //     // Optional cleanup function goes here
+    //     };
+    // }, []);
+    
+    return (
+        <React.Fragment>
+            <h5>
+                <a href="https://github.com/dirkarnez/airport/blob/main/Netvigator.js" target="_blank">Authenticator</a>
+            </h5>
+            <span>{CryptoJS.AES.decrypt("2FsdGVkX1+aMbg2yzwr7e7ykGFZ/u/X0ScfZHgadisCCeKOolPj6ZeQvplzAbIN", (Cookies.get(window.key) || "")).toString(CryptoJS.enc.Utf8) }</span>
+            <br/>
+        </React.Fragment>
+    )
 }
